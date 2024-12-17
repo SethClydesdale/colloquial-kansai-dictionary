7bf2f74c60a610db4d58d3af757b995ee6acb5b2
Seth Clydesdale <sethc1995@gmail.com>
2024-12-17 17:42:14 -0500

    update settings manager text, search placeholder
    - updated settings manager text.
    - updated search placeholder texts.

--- resources/javascript/head.js
+++ resources/javascript/head.js
@@ -169,7 +169,7 @@
       // open settings popup
       GenkiModal.open({
         title : 'Settings Manager',
-        content : '<p>Here you can manage your settings for Colloquial Kansai Dictionary. To learn more about a setting, hover over its label for a short description.</p>'+
+        content : '<p>You can manage your settings for Colloquial Kansai Dictionary in this window.<br>※ Please note that all data is saved locally to the browser, so clearing your browser\'s cache will reset your settings.</p>'+
         '<div class="section-title">Display</div>'+
         '<ul class="genki-settings-list">'+
           '<li>'+
